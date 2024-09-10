f414af5aa194d351c3685fef0860dd7106065bfb
Pier Luigi Fiorini <p.fiorini@teamsystem.com>
2024-09-10 15:39:16 +0000

    new app async
    Co-authored-by: Federico Villa <f.villa@teamsystem.com>
    Co-authored-by: ntanzarella <ntanzarella@users.noreply.github.com>

--- first-app-async/app/index.js
+++ first-app-async/app/index.js
@@ -19,9 +19,10 @@ function otherIncomingRequests() {
  * @param {*} n
  * @returns
  */
-function longRequest(n) {
+function longRequestOrig(n) {
     let id = i++;
     console.log(`Id: ${id}. Starting blocking request. Find primes from 2 to ${n}`);
+
     const start = new Date();
 
     const primes = mathUtils.getPrimeNumbersWithinRange(2, n);
@@ -30,4 +31,28 @@ function longRequest(n) {
     console.log(`Id: ${id}. Finished blocking request. Elapsed ms: ${end.getTime() - start.getTime()}`);
 
     return primes;
+}
+
+async function longRequest(n) {
+    primes = [];
+
+    let id = i++;
+    console.log(`Id: ${id}. Starting non-blocking request. Find primes from 2 to ${n}`);
+
+    for (i = 2; i <= n; i += 10) {
+        setTimeout(() => {
+            const x = i;
+            const y = i + 9;
+            console.log(`Id: ${id}. Elaborating primes from ${x} to ${y}`);
+            const start = new Date();
+            primes += mathUtils.getPrimeNumbersWithinRange(x, y);
+            const end = new Date();
+            console.log(`Id: ${id}. Finished blocking request. Elapsed ms: ${end.getTime() - start.getTime()}`);
+        }, 50);
+
+        console.log(`Id: ${id}. Suspended non-blocking request`);
+        await new Promise(r => setTimeout(r, 50));
+    }
+
+    console.log("primse", primes);
 }
